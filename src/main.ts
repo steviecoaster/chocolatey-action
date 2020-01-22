e26076e1435091069408fdb882d9ec1d2fcd403f
Stephen Valdinger <stephen@chocolatey.io>
2020-01-22 10:50:43 -0800

    try this again with a new yml

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import * as exec from '@actions/exec'
 async function run(): Promise<void> {
   try {
     await exec.exec(
-      `choco install vswhere -y`
+      /*`choco install vswhere -y`*/
+      choco install vswhere -y
     )
   } catch (error) {
     core.setFailed(error.message)
